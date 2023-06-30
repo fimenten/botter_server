8e48aa6ea65982710b57f024c6b90657d12a2d75
tatsuya <tungsten.tatsuya@gmail.com>
2023-07-01 03:10:20 +0900

    ssssd

--- app.js
+++ app.js
@@ -1,84 +1,83 @@
-// Fetch orders data from the backend API
-function fetchOrders() {
-    fetch('/getActiveOrders', {
+const apiUrl = 'http://localhost:893';
+
+// Function to fetch active orders from the API
+async function fetchActiveOrders(strategyId) {
+  try {
+    const response = await fetch(`${apiUrl}/getActiveOrders`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ strategy_id: strategyId })
+    });
+    const data = await response.json();
+    return data.order_id_list;
+  } catch (error) {
+    console.error('Error fetching active orders:', error);
+    return [];
+  }
+}
+
+// Function to filter orders based on conditions
+async function filterOrders(strategyId, filterOptions) {
+  try {
+    const response = await fetch(`${apiUrl}/filterOrders`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        strategy_id: 'your_strategy_id' // Replace with the actual strategy ID
+        strategy_id: strategyId,
+        active: filterOptions.active,
+        exchange: filterOptions.exchange,
+        ticker: filterOptions.ticker
       })
-    })
-    .then(response => response.json())
-    .then(data => {
-      // Update the table with the orders data
-      updateTable(data.order_id_list);
-    })
-    .catch(error => {
-      console.error('Error fetching orders:', error);
     });
+    const data = await response.json();
+    return data.filtered_order_ids;
+  } catch (error) {
+    console.error('Error filtering orders:', error);
+    return [];
   }
-  
-  // Update the table with the orders data
-  function updateTable(orderIds) {
-    const tableBody = document.querySelector('#orders-table tbody');
-    tableBody.innerHTML = '';
-  
-    orderIds.forEach(orderId => {
-      // Create a new row for each order
-      const row = document.createElement('tr');
-      
-      // Add the order details as table cells
-      row.innerHTML = `
-        <td>${orderId}</td>
-        <td>Exchange</td>
-        <td>Ticker</td>
-        <td>Side</td>
-        <td>Size</td>
-        <td>Order Type</td>
-        <td>Status</td>
-        <td>Received Timestamp</td>
-        <td>Send Datetime</td>
-      `;
-  
-      tableBody.appendChild(row);
-    });
-  }
-  
-  // Filter orders based on the input values
-  function filterOrders() {
-    const activeFilter = document.querySelector('#active-filter').checked;
-    const exchangeFilter = document.querySelector('#exchange-filter').value.trim();
-    const tickerFilter = document.querySelector('#ticker-filter').value.trim();
-  
-    fetch('/filterOrders', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        strategy_id: 'your_strategy_id', // Replace with the actual strategy ID
-        active: activeFilter,
-        exchange: exchangeFilter,
-        ticker: tickerFilter
-      })
-    })
-    .then(response => response.json())
-    .then(data => {
-      // Update the table with the filtered orders data
-      updateTable(data.filtered_order_ids);
-    })
-    .catch(error => {
-      console.error('Error filtering orders:', error);
-    });
-  }
-  
-  // Attach event listeners to the filter button and input fields
-  document.addEventListener('DOMContentLoaded', () => {
-    const filterBtn = document.querySelector('#filter-btn');
-    filterBtn.addEventListener('click', filterOrders);
-  
-    // Fetch all orders initially
-    fetchOrders();
+}
+
+// Function to render orders table
+function renderOrdersTable(orderIds) {
+  const tableBody = document.getElementById('orders-table-body');
+  tableBody.innerHTML = '';
+
+  orderIds.forEach(orderId => {
+    const row = document.createElement('tr');
+    row.innerHTML = `<td>${orderId}</td>`;
+    tableBody.appendChild(row);
   });
-  
+}
+
+// Function to handle filter form submission
+function handleFilterSubmit(event) {
+  event.preventDefault();
+
+  const strategyId = document.getElementById('strategy-id-input').value;
+  const activeFilter = document.getElementById('active-filter').checked;
+  const exchangeFilter = document.getElementById('exchange-filter').value;
+  const tickerFilter = document.getElementById('ticker-filter').value;
+
+  const filterOptions = {
+    active: activeFilter,
+    exchange: exchangeFilter,
+    ticker: tickerFilter
+  };
+
+  filterOrders(strategyId, filterOptions)
+    .then(filteredOrderIds => renderOrdersTable(filteredOrderIds))
+    .catch(error => console.error('Error filtering orders:', error));
+}
+
+// Function to initialize the app
+function init() {
+  const filterForm = document.getElementById('filter-form');
+  filterForm.addEventListener('submit', handleFilterSubmit);
+}
+
+// Initialize the app when the DOM is ready
+document.addEventListener('DOMContentLoaded', init);
